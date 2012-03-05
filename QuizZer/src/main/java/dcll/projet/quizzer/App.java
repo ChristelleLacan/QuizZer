b2b8cfb00ba6cc931ef52090535ff9f72ec871f1
Christelle Lacan <christelle.lacan@gmail.com>
2012-03-05 16:29:23 +0100

    Commit de Chris

--- QuizZer/src/main/java/dcll/projet/quizzer/App.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Clement.Hello World From Eros " );
+        System.out.println( "Hello World! From Clement.Hello World From Eros from Chris" );
     }
 }
