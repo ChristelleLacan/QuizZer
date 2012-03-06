93a1577e3d49689bfcf2315a480449b60f1d7ba0
Clement Bardou <etu2@ensdl-02.edu.ups-tlse.fr>
2012-03-06 09:43:46 +0100

    ajout de quiz de test + chargement d'un doc XML

--- QuizZer/src/main/java/dcll/projet/quizzer/XmlToJava.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/XmlToJava.java
@@ -1,5 +1,11 @@
 package dcll.projet.quizzer;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 
 //
 //
@@ -10,10 +16,28 @@ package dcll.projet.quizzer;
 //
 //
 
-
-
-
 public class XmlToJava {
-	 	
+
+	private org.jdom.Document document;
+	private Element racine;
+
+	public void loadDocument() {
+		// On crée une instance de SAXBuilder
+		SAXBuilder sxb = new SAXBuilder();
+		try {
+			// On crée un nouveau document JDOM avec en argument le fichier XML
+			document = sxb.build(this.getClass().getResourceAsStream("../xmldoc/quiz.xml"));
+			// Le parsing est terminé ;)
+		} catch (Exception e) {
+		}
+
+		// On initialise un nouvel élément racine avec l'élément racine du
+		// document.
+		racine = document.getRootElement();
+	}
+
+	public void run() {
+		
+	}
 
 }
