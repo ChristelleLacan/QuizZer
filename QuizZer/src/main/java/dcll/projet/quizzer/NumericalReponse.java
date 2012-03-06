8632778aae91903dcb6b3387003dba56f3688e62
LUCE <etu2@ensdl-11.edu.ups-tlse.fr>
2012-03-06 10:00:54 +0100

    #<5>:modif déclaration attributs

--- QuizZer/src/main/java/dcll/projet/quizzer/NumericalReponse.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/NumericalReponse.java
@@ -15,4 +15,11 @@ public class NumericalReponse extends Question {
 	
 	private Question numerical;
 	private String text;
+	private String name;
+	private String questionText;
+	private int defaultgrade;
+	private double penalty;
+	private boolean shuffleanswers;
+	private boolean hidden;
+	
 }
