9d866a5e02d3122fde7b7c026f010c92182c89ec
DELL <DELL@DELL-PC>
2012-03-15 13:47:47 +0100

    #5 Classe "NumericalReponse" : Finalisée

--- QuizZer/src/main/java/dcll/projet/quizzer/NumericalReponse.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/NumericalReponse.java
@@ -12,41 +12,18 @@ import java.util.List;
 
 public class NumericalReponse extends Question {
 
-	private String text;
-	private String name;
-	private String questionText;
-	private int defaultgrade;
-	private double penalty;
-	private boolean shuffleanswers;
-	private boolean hidden;
-	private String comment;
-	private String type;
 
 	public NumericalReponse(String mName, String mQuestionText,
 			int mDefaultgrade, double mPenalty, boolean mShuffleanswers,
 			boolean mHidden, List<Answer> mAnswers, String mFormat) {
 		super(mName, mQuestionText, mDefaultgrade, mPenalty, mShuffleanswers,
 				mHidden, mAnswers, mFormat);
+		type = "NumericalReponse";
 		// TODO Auto-generated constructor stub
 	}
-
-	public double getPenalty() {
-		return penalty;
+	
+	public String toString(){
+		return "Ceci est : une NumericalReponse  \n";
 	}
 
-	public int getDefaultgrade() {
-		return defaultgrade;
-	}
-
-	public boolean isHidden() {
-		return hidden;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public String getText() {
-		return text;
-	}
 }
