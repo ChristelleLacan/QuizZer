8068504a18d45b41591fbecf0a46c9ef9d4c2765
Christelle <Christelle@130.120.242.197>
2012-03-16 15:25:01 +0100

    #6 Modification de la classe Answer

--- QuizZer/src/main/java/dcll/projet/quizzer/Answer.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/Answer.java
@@ -3,42 +3,11 @@ package dcll.projet.quizzer;
 public class Answer {
 	private String type;
 	private String question;
-
-	public String getQuestion() {
-		return question;
-	}
-	public void setQuestion(String question) {
-		this.question = question;
-	}
-	private String text;
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public int getFraction() {
-		return fraction;
-	}
-	public void setFraction(int fraction) {
-		this.fraction = fraction;
-	}
-	public String getFeedback() {
-		return feedback;
-	}
-	public void setFeedback(String feedback) {
-		this.feedback = feedback;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	private int fraction;
+	private float fraction;
 	private String feedback;
+	private String text;
 	
-	public Answer(String text, int fraction, String feedback) {
+	public Answer(String text, float fraction, String feedback) {
 		this.text = text;
 		this.fraction = fraction;
 		this.feedback = feedback;
