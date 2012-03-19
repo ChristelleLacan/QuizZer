e9cee53f3a7fd4ce575f2159fc1976e520c2e271
Clement Bardou <clement.b81@gmail.com>
2012-03-19 16:16:59 +0100

    #9 little correction !

--- QuizZer/src/main/java/dcll/projet/quizzer/App.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/App.java
@@ -2,17 +2,14 @@ package dcll.projet.quizzer;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	String myFile = "../xmldoc/quiz.xml";
-        IxmlToJava myXmlToJavaparser = new XmlToJava();
-        Questionnaire myQuiz = myXmlToJavaparser.run(myFile);
-        myQuiz.toString();
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		String myFile = "../xmldoc/quiz.xml";
+		XmlToJava myXmlToJavaParser = new XmlToJava();
+		Questionnaire myQuiz = myXmlToJavaParser.run(myFile);
+
+		System.out.println(myQuiz);
+	}
 }
--- QuizZer/src/main/java/dcll/projet/quizzer/IxmlToJava.java
+++ QuizZer/src/main/java/dcll/projet/quizzer/IxmlToJava.java
@@ -1,9 +1,10 @@
 package dcll.projet.quizzer;
 
 public interface IxmlToJava {
+
 	/**
-	 * This method is used to load xml document myFile and launch the parsing of the
-	 * xml document
+	 * This method is used to load xml document myFile and launch the parsing of
+	 * the xml document
 	 * 
 	 * @param myFile
 	 * @return quiz
